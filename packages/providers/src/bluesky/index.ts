15c89a03e00ea0e263338f1ce4070b165987ff3b
MahanMahur <mahanmahur.email@gmail.com>
2026-08-30 22:15:49 +0330

    feat(providers): public surface

---
+++ packages/providers/src/bluesky/index.ts
@@ -0,0 +1,4 @@
+export { BlueskyProvider, toProviderError } from './adapter.js'
+export { capabilities, limits, media, text } from './capabilities.js'
+export { detectFacets, byteLength, byteOffsetOf } from './facets.js'
+export type { Facet } from './facets.js'
