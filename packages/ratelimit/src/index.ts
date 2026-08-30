a03f03999c5d88a790b1fa2f0bcd55f40436d338
MahanMahur <mahanmahur.email@gmail.com>
2026-08-30 22:15:39 +0330

    feat(ratelimit): public surface

---
+++ packages/ratelimit/src/index.ts
@@ -0,0 +1,3 @@
+export { RateLimiter, AccountMutex } from './bucket.js'
+export type { BudgetSpec, AcquireResult, OperationClass, BudgetScope } from './bucket.js'
+export { ACQUIRE, REFUND, PENALISE, RECOVER } from './scripts.js'
