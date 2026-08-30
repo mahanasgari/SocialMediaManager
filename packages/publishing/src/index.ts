9abc6ccb5bcea6b0de627ed02d6066d72ce64d62
MahanMahur <mahanmahur.email@gmail.com>
2026-08-30 22:15:42 +0330

    feat(publishing): public surface

---
+++ packages/publishing/src/index.ts
@@ -0,0 +1,23 @@
+export {
+  derivePostStatus,
+  describeStatus,
+  shouldDerive,
+  VARIANT_STATUSES,
+  POST_STATUSES,
+  EDITORIAL_STATUSES,
+  TERMINAL_VARIANT,
+} from './status.js'
+export type { VariantStatus, PostStatus } from './status.js'
+
+export {
+  idempotencyKey,
+  fingerprintFor,
+  findMatch,
+  similarity,
+  normaliseForMatch,
+  serialiseFingerprint,
+  parseFingerprint,
+} from './fingerprint.js'
+export type { Fingerprint, RemoteCandidate, MatchOptions } from './fingerprint.js'
+
+export { Publisher } from './pipeline.js'
